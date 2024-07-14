7f2cdd364f4fc42fa350cc722d0b113df6c64814
arun dev <4rund3v@gmail.com>
2024-07-14 11:53:11 +0530

    Refactored usign the rustfmt command

--- basics/common_programming_concepts/src/main.rs
+++ basics/common_programming_concepts/src/main.rs
@@ -1,5 +1,4 @@
-
-const MAX_TIME_TO_LIVE: u16 = 60*30;
+const MAX_TIME_TO_LIVE: u16 = 60 * 30;
 
 // Function to test out how variables behave
 fn variables() {
@@ -13,7 +12,7 @@ fn variables() {
     println!(" The initial value of a is :: {a}");
 
     {
-        let a = a* a ;
+        let a = a * a;
         println!("The value of a in the inner block 1 is :: {a}");
     }
     println!("The value of a in the outer block is :: {a}");
@@ -25,12 +24,12 @@ fn variables() {
 fn understanding_variables_data_types() {
     let radius: u8 = 22;
     println!("The value of the radius is :: {radius}");
-    let  circumfrence: u16 = 21_999;
+    let circumfrence: u16 = 21_999;
     println!("The circumfrence of the circle is:: {circumfrence}");
     let pi: f32 = 3.1414972;
     println!("The value of the pi is :: {pi}");
     let pi_root: f64 = (pi as f64).sqrt();
-    println!("The value of the pi_root is :: {pi_root}");   
+    println!("The value of the pi_root is :: {pi_root}");
     circle_details();
     let flag: bool = true;
     println!("The value of the flag is :: {flag}");
@@ -46,7 +45,7 @@ fn circle_details() {
     let radius: u16 = 43;
     let pi: f32 = 3.1413570;
     let circumfrence: f32 = (2 as f32) * pi * (radius as f32);
-    let area: f32 = pi * (( radius * radius ) as f32);
+    let area: f32 = pi * ((radius * radius) as f32);
     println!("The circle radius is : {radius}");
     println!("The circumfrence is  : {circumfrence}");
     println!("The circle area  is  : {area}");
@@ -63,15 +62,19 @@ fn rhombus_details() {
     let p3: (i16, u16) = (8, 0);
     let p4: (u16, i16) = (0, -10);
 
-    let diagonal_squared: i16 = i16::pow((p1.0 as i16) - p3.0, 2) + i16::pow((p1.1 as i16) - (p3.1 as i16), 2);
+    let diagonal_squared: i16 =
+        i16::pow((p1.0 as i16) - p3.0, 2) + i16::pow((p1.1 as i16) - (p3.1 as i16), 2);
     let diagonal: f32 = (diagonal_squared as f32).sqrt();
-    println!("The points of the rhombus is :: 
+    println!(
+        "The points of the rhombus is :: 
         P1 : {:?}
         P2 : {:?}
         P3 : {:?}
         P4 : {:?}
-        Diagonal length is : {:?}", p1, p2, p3, p4, diagonal);
-    let p1_copy:(i16, u16) = p1;
+        Diagonal length is : {:?}",
+        p1, p2, p3, p4, diagonal
+    );
+    let p1_copy: (i16, u16) = p1;
     println!("The copy of the p1 is :{:?}", p1_copy);
     // Destructuring an tuple
     let (p1_x, p1_y) = p1;
@@ -86,28 +89,37 @@ fn rhombus_details() {
     println!("The p3 point is :: {p3_point}");
 }
 
-
-fn distance_between_points(px:(i32, i32), py:(i32, i32)) -> f32{
+fn distance_between_points(px: (i32, i32), py: (i32, i32)) -> f32 {
     println!("The distance between points {:?} to {:?}", px, py);
-    let dist_between_xy: f32 = f32::sqrt( (i32::pow(py.0 - px.0, 2) + i32::pow(py.1 - px.1, 2) ) as f32);
+    let dist_between_xy: f32 =
+        f32::sqrt((i32::pow(py.0 - px.0, 2) + i32::pow(py.1 - px.1, 2)) as f32);
     println!("The distance calculated is :: {dist_between_xy}");
     dist_between_xy
 }
 
-
-fn geometry_methods(mut point_u: (i32, i32), point_v: (i32, i32), point_x: (i32, i32), point_y: (i32, i32)) {
+fn geometry_methods(
+    mut point_u: (i32, i32),
+    point_v: (i32, i32),
+    point_x: (i32, i32),
+    point_y: (i32, i32),
+) {
     point_u.0 = point_u.0 + point_u.0;
-    println!("The points provided are  
+    println!(
+        "The points provided are  
         Point u : {:?}
         Point v : {:?}
         Point x : {:?}
         Point y : {:?}
-        ", point_u, point_v, point_x, point_y);
-    let calculated_dist : f32 = distance_between_points(point_x, point_y);
-    println!("The distance calucated between the points is  {:?}",calculated_dist );
+        ",
+        point_u, point_v, point_x, point_y
+    );
+    let calculated_dist: f32 = distance_between_points(point_x, point_y);
+    println!(
+        "The distance calucated between the points is  {:?}",
+        calculated_dist
+    );
     trignomentry_understanding();
 }
-
 
 fn trignomentry_understanding() {
     let triangle: (i32, i32, i32) = (3, 4, 5);
@@ -121,26 +133,29 @@ fn trignomentry_understanding() {
     } else {
         hypotenuse = f32::sqrt((i32::pow(triangle.1, 2) + i32::pow(triangle.2, 2)) as f32);
     }
-    println!("The hypotenuse of the triangle : {:?} is {}", triangle, hypotenuse);
+    println!(
+        "The hypotenuse of the triangle : {:?} is {}",
+        triangle, hypotenuse
+    );
     if hypotenuse == triangle.2 as f32 {
-        println!("The hypotenuse was calculated correctly. {:?}", hypotenuse );
+        println!("The hypotenuse was calculated correctly. {:?}", hypotenuse);
     }
 }
 
-
 fn understanding_loops() {
-    let mut balance : i32 = 100;
+    let mut balance: i32 = 100;
 
     let remaining = loop {
         balance -= 10;
         if balance <= 10 {
-            break balance *3; // the semicolon here is kinda optional, since its the last statement 
+            break balance * 3; // the semicolon here is kinda optional, since its the last statement
         }
     };
-    println!("The remaining amount is {:?} from balance : {balance}", remaining);
-        
+    println!(
+        "The remaining amount is {:?} from balance : {balance}",
+        remaining
+    );
 }
-
 
 fn main() {
     println!("Hello, world!, lets understand rust");
@@ -150,8 +165,8 @@ fn main() {
     println!("The max time to live is :: {MAX_TIME_TO_LIVE}");
     println!("The functions block starts here:: ");
     let points = [(32, 36), (40, 50), (38, 44), (48, 58)];
-    println!("The points defined are {:?}",points);
+    println!("The points defined are {:?}", points);
     geometry_methods(points[0], points[1], points[2], points[3]);
-    println!("The points defined are {:?}",points);
+    println!("The points defined are {:?}", points);
     understanding_loops();
 }
