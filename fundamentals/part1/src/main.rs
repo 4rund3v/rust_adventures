a1bd06518a71b53249dbc0247e82675c87d081af
arun dev <4rund3v@gmail.com>
2024-08-21 10:13:01 +0530

    Updated the logic to use structs to represent the expense.

--- fundamentals/part1/src/main.rs
+++ fundamentals/part1/src/main.rs
@@ -8,6 +8,8 @@
  *  - Takes expenses in a day, calculates mean median mode
  *  - Calculates the current balances/based on expenses
  */
+// Use the `use` keyword to import the fmt module from std.
+use std::fmt;
 
 #[derive(Debug)]
 struct Expense {
@@ -16,8 +18,20 @@ struct Expense {
     reason: String,
 }
 
+impl fmt::Display for Expense {
+    // This enables the use of the {} for the Expense structure.
+    // The fmt display method expects the following format
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "<Expense: {} price:[{:0>7}] for reason: {:?}>",
+            self.expense_type, self.amount, self.reason
+        )
+    }
+}
+
 fn calculate_day_expenses() {
-    let daily_expenses: [Expense; 4] = [
+    let daily_expenses: [Expense; 5] = [
         Expense {
             expense_type: String::from("expense"),
             amount: 40,
@@ -38,17 +52,26 @@ fn calculate_day_expenses() {
             amount: 30,
             reason: String::from("Shuttle Cock"),
         },
+        Expense {
+            expense_type: String::from("expense"),
+            amount: 100,
+            reason: String::from("Bannana and Grapes."),
+        },
     ];
     let mut daily_sum: i32 = 0;
     for expense in daily_expenses {
-        println!("The day expense to be processed is :: {:#?}", expense);
-        let Expense {expense_type: day_expense_type, amount: day_amount, reason: day_reason} = expense;
+        println!("The day expense to be processed is :: {}", expense);
+        // unpacking the struct into variables
+        let Expense {
+            expense_type: day_expense_type,
+            amount: day_amount,
+            reason: day_reason,
+        } = expense;
         daily_sum += day_amount as i32;
         println!("The {day_expense_type} reason is :: {day_reason} and it costed :: {day_amount}");
     }
     println!("The day expenses calculated is :: {daily_sum}");
 }
-
 
 fn main() {
     println!("Calculating the day expenses ");
